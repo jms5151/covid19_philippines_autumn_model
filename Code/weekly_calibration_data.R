94d934ce5c30df37fdcb49a583b3b95f56bf7c27
jms5151 <jamie.sziklay@gmail.com>
2020-08-14 19:18:23 +1000

    update region names and upload date.

--- Code/weekly_calibration_data.R
+++ Code/weekly_calibration_data.R
@@ -7,14 +7,14 @@ library(tidyquant)
 library(ggplot2)
 
 # load data
-uploadDate <- "2020-07-28"
+uploadDate <- "2020-08-08"
 fileName <- paste0("FASSSTER_Data/ConfirmedCases_Final_", uploadDate, ".csv")
 fassster_data <- read.csv(fileName, head = T, stringsAsFactors = F) 
 
-uploadDate2 <- "20200728"
+uploadDate2 <- "20200809"
 fileName2 <- paste0("DOH_COVID_Data_Drop/DOH COVID Data Drop_ ", uploadDate2, " - 05 DOH Data Collect - Daily Report.csv")
 doh_data <- read.csv(fileName2, head = T, stringsAsFactors = F) 
-  
+
 # format dates
 fassster_data$imputed_Date_Admitted <- as.Date(fassster_data$imputed_Date_Admitted, "%Y-%m-%d")
 fassster_data$Date_Died <- as.Date(fassster_data$Date_Died, "%Y-%m-%d")
@@ -22,28 +22,29 @@ doh_data$reportdate <- as.Date(doh_data$reportdate, "%Y-%m-%d")
 startdate <- as.Date("2019-12-31", "%Y-%m-%d")
 
 # format region names
-fassster_data$Region[fassster_data$Region == "4A"] <- "CAL"
-fassster_data$Region[fassster_data$Region == "07"] <- "CEN-VIS"
+fassster_data$Region[fassster_data$Region == "NCR"] <- "manila"
+fassster_data$Region[fassster_data$Region == "4A"] <- "calabarzon"
+fassster_data$Region[fassster_data$Region == "07"] <- "central-visayas"
 
 doh_data$Region <- doh_data$region
-doh_data$Region[doh_data$Region == "NATIONAL CAPITAL REGION (NCR)"] <- "NCR"
-doh_data$Region[doh_data$Region == "REGION IV-A (CALABAR ZON)"] <- "CAL"
-doh_data$Region[doh_data$Region == "REGION VII (CENTRAL VISAYAS)"] <- "CEN-VIS"
+doh_data$Region[doh_data$Region == "NATIONAL CAPITAL REGION (NCR)"] <- "manila"
+doh_data$Region[doh_data$Region == "REGION IV-A (CALABAR ZON)"] <- "calabarzon"
+doh_data$Region[doh_data$Region == "REGION VII (CENTRAL VISAYAS)"] <- "central-visayas"
 
 # duplicate data and combine with original dataset to calculate national and regional estimates simultaneously
-dup_fassster_data <- data
-dup_fassster_data$Region <- "PHL"
+dup_fassster_data <- fassster_data
+dup_fassster_data$Region <- "philippines"
 fassster_data2 <- rbind(fassster_data, dup_fassster_data)
 
 dup_doh_data <- doh_data
-dup_doh_data$Region <- "PHL"
+dup_doh_data$Region <- "philippines"
 doh_data2 <- rbind(doh_data, dup_doh_data)
 
 # aggregate notification data to weekly values 
 notifications <- fassster_data2 %>%
   rename(Date = imputed_Date_Admitted) %>%
   filter(Date <= as.Date(uploadDate, "%Y-%m-%d")) %>%
-  filter(Region == "PHL" | Region == "NCR" | Region == "CAL" | Region == "CEN-VIS") %>% 
+  filter(Region == "philippines" | Region == "manila" | Region == "calabarzon" | Region == "central-visayas") %>% 
   group_by(Region, Date) %>%
   summarise(values = length(Date)) %>%
   tq_transmute(select     = values,
@@ -54,7 +55,7 @@ notifications <- fassster_data2 %>%
 deaths <- fassster_data2 %>%
   rename(Date = Date_Died) %>%
   filter(Date <= as.Date(uploadDate, "%Y-%m-%d")) %>%
-  filter(Region == "PHL" | Region == "NCR" | Region == "CAL" | Region == "CEN-VIS") %>% 
+  filter(Region == "philippines" | Region == "manila" | Region == "calabarzon" | Region == "central-visayas") %>% 
   group_by(Region, Date) %>%
   summarise(values = length(Date)) %>%
   tq_transmute(select     = values,
@@ -65,7 +66,7 @@ deaths <- fassster_data2 %>%
 icu <- doh_data2 %>%
   rename(Date = reportdate) %>%
   filter(Date <= as.Date(uploadDate, "%Y-%m-%d")) %>%
-  filter(Region == "PHL" | Region == "NCR" | Region == "CAL" | Region == "CEN-VIS") %>% 
+  filter(Region == "philippines" | Region == "manila" | Region == "calabarzon" | Region == "central-visayas") %>% 
   group_by(Region, Date) %>%
   summarise(values = sum(icu_o)) %>%
   tq_transmute(select     = values,
@@ -90,7 +91,7 @@ for(i in 1:length(dfs)){
     theme_bw() +
     ylab(paste0("Mean ", names[i], " per week")) +
     xlab("Date")
-  ggsave(paste0("Figures/", names[i], "_by_region_", uploadDate, ".tiff"))
+  ggsave(paste0("Figures/", uploadDate, "_", names[i], "_by_region_", ".tiff"))
   # subset by region
   for(j in regions){
     dfx2 <- subset(dfx, Region == j)
@@ -100,10 +101,10 @@ for(i in 1:length(dfs)){
     values_wide <- paste("[", dfx2$values, "]", collapse=",")
     values_wide <- gsub(" ", "", values_wide)
     plot_targets <- c(times_wide, values_wide)
-    writeLines(plot_targets, paste0("Output/", j, "_", names[i], "_wide_", uploadDate, ".txt"))
+    writeLines(plot_targets, paste0("Output/", uploadDate, "_", j, "_", names[i], "_wide.txt"))
     # format and save data for calibration targets
     dfx2$times <- paste0(dfx2$times, ",")
     dfx2$values <- paste0(dfx2$values, ",")
-    write.csv(dfx2[,c("times", "values")], paste0("Output/", j, "_", names[i], "_long_", uploadDate, ".csv"), row.names = F)
+    write.csv(dfx2[,c("times", "values")], paste0("Output/", uploadDate, "_", j, "_", names[i], "_long.csv"), row.names = F)
   }
 }
