ea8bb0d076e707825684a7eff917bbb967f7d211
Jamie M. Caldwell <jamie.sziklay@gmail.com>
2020-09-01 15:39:51 +1000

    change daily deaths to cumulative deaths and daily icu o to most recent icu o.

--- Code/calibration_data_7d_rolling_window.R
+++ Code/calibration_data_7d_rolling_window.R
@@ -7,11 +7,11 @@ library(zoo)
 library(ggplot2)
 
 # load data
-uploadDate <- "2020-08-22"
+uploadDate <- "2020-08-31"
 fileName <- paste0("FASSSTER_Data/ConfirmedCases_Final_", uploadDate, ".csv")
 fassster_data <- read.csv(fileName, head = T, stringsAsFactors = F) 
 
-uploadDate2 <- "20200823"
+uploadDate2 <- "20200831"
 fileName2 <- paste0("DOH_COVID_Data_Drop/DOH COVID Data Drop_ ", uploadDate2, " - 05 DOH Data Collect - Daily Report.csv")
 doh_data <- read.csv(fileName2, head = T, stringsAsFactors = F) 
 
@@ -50,59 +50,50 @@ notifications <- fassster_data2 %>%
   complete(Date = seq.Date(min(Date), max(Date), by="day")) %>%
   mutate(values = round(rollapply(values, width = 7, FUN = mean, align = "left", fill = NA, na.rm = T)))
 
-# aggregate death data to weekly values 
+notifications$values <- round(notifications$values)
+notifications$times <- as.numeric(difftime(notifications$Date, startdate, units = "days"))
+
+# plot
+fig <- ggplot(data=notifications, aes(x=Date, y=values, group=Region)) +
+  geom_line()+
+  geom_point() +
+  facet_wrap(~Region, scales="free") +
+  theme_bw() +
+  ylab("Mean daily cases") +
+  xlab("Date")
+ggsave(paste0("Figures/", uploadDate, "_notifications_by_region.tiff"))
+
+# subset and save data by region
+regions <- unique(notifications$Region)
+
+for(j in 1:length(regions)){
+  dfx2 <- subset(notifications, Region == regions[j])
+  dfx2 <- dfx2[complete.cases(dfx2),]
+  # format and save data for calibration targets
+  dfx2$times <- paste0(dfx2$times, ",")
+  dfx2$values <- paste0(dfx2$values, ",")
+  write.csv(dfx2[,c("times", "values")], paste0("Output/", uploadDate, "_", regions[j], "_notifications_long.csv"), row.names = F)
+}
+
+# cumulative death data by region
 deaths <- fassster_data2 %>%
   rename(Date = Date_Died) %>%
   filter(Date <= as.Date(uploadDate, "%Y-%m-%d")) %>%
   filter(Region == "philippines" | Region == "manila" | Region == "calabarzon" | Region == "central-visayas") %>% 
-  group_by(Region, Date) %>%
-  summarise(values = length(Date)) %>%
-  complete(Date = seq.Date(min(Date), max(Date), by="day")) %>%
-  mutate(values = round(rollapply(values, width = 7, FUN = mean, align = "left", fill = NA, na.rm = T)))
+  group_by(Region) %>%
+  summarise(values = length(Date), Date = max(Date)) 
 
+deaths$times <- as.numeric(difftime(deaths$Date, startdate, units = "days"))
+write.csv(deaths, paste0("Output/", uploadDate, "_deaths.csv"))
 
-# aggregate ICU data to weekly values 
+# most recent ICU data 
 icu <- doh_data2 %>%
   rename(Date = reportdate) %>%
   filter(Date <= as.Date(uploadDate, "%Y-%m-%d")) %>%
   filter(Region == "philippines" | Region == "manila" | Region == "calabarzon" | Region == "central-visayas") %>% 
   group_by(Region, Date) %>%
   summarise(values = sum(icu_o, na.rm = T)) %>%
-  complete(Date = seq.Date(min(Date), max(Date), by="day")) %>%
-  mutate(values = round(rollapply(values, width = 7, FUN = mean, align = "left", fill = NA, na.rm = T)))
+  filter(Date == max(Date))
 
-# format, plot, and save data
-dfs <- list(notifications, deaths, icu)
-names <- c("notifications", "deaths", "icu")
-regions <- c("calabarzon", "central-visayas", "manila", "philippines")
-deathtimes <- c(153, 92, 183, 153)
-
-for(i in 1:length(dfs)){
-  # subset by calibration target
-  dfx <- dfs[[i]]
-  dfx$values <- round(dfx$values)
-  dfx$times <- as.numeric(difftime(dfx$Date, startdate, units = "days"))
-  # plot
-  fig <- ggplot(data=dfx, aes(x=Date, y=values, group=Region)) +
-    geom_line()+
-    geom_point() +
-    facet_wrap(~Region, scales="free") +
-    theme_bw() +
-    ylab(paste0("Mean daily", names[i])) +
-    xlab("Date")
-  ggsave(paste0("Figures/", uploadDate, "_", names[i], "_by_region.tiff"))
-  # subset by region
-  for(j in 1:length(regions)){
-    dfx2 <- subset(dfx, Region == regions[j])
-    if(names[i] == "deaths"){
-      dfx2 <- subset(dfx2, times > deathtimes[j] & times < max(times)-13)  
-    } else {
-      dfx2 <- subset(dfx2, times > 60 & times < max(times)-13)  
-    }
-    dfx2 <- dfx2[complete.cases(dfx2),]
-    # format and save data for calibration targets
-    dfx2$times <- paste0(dfx2$times, ",")
-    dfx2$values <- paste0(dfx2$values, ",")
-    write.csv(dfx2[,c("times", "values")], paste0("Output/", uploadDate, "_", regions[j], "_", names[i], "_long.csv"), row.names = F)
-  }
-}
+icu$times <- as.numeric(difftime(icu$Date, startdate, units = "days"))
+write.csv(icu, paste0("Output/", uploadDate, "_icu.csv"))
